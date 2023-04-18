15b339e608f5f0b585341a0746b6f5405469ad3c
CharlieH7 <hope91824@gmail.com>
2023-04-18 20:25:42 +1000

    added quantity in the cart

--- addToCart.js
+++ addToCart.js
@@ -1,72 +1,74 @@
-// // add to cart function
-// var addItemId = 0;
-// function addToCart(item) {
-//   var selectedItem = document.createElement('div');
-//   selectedItem.classList.add('cartImg');
-//   selectedItem.setAttribute('id', addItemId);
-//   var img = document.createElement('img');
-//   img.setAttribute('src', item.children[0].currentSrc);
-//   var title = document.createElement('h3');
-//   title.innerText = item.children[1].innerText;
-//   var label = document.createElement('label');
-//   label.innerText = item.children[2].children[0].innerText;
-//   var select  = document.createElement('select');
-//   select.innerText = item.children[2].children[1].value;
-//   label.append(select);
-//   var cartItems = document.getElementById('title');
-//   selectedItem.append(img);
-//   selectedItem.append(title);
-//   cartItems.append(selectedItem);
-// }
-
 function addToCart(item) {
-    // get the item details
-    var itemImage = item.querySelector("img").src;
-    var itemName = item.querySelector("h3").innerText;
-    var itemPrice = item.querySelector(".price").innerText;
-  
+  // get the item details
+  var itemImage = item.querySelector("img").src;
+  var itemName = item.querySelector("h3").innerText;
+  var itemPrice = item.querySelector(".price").innerText;
+
+  // check if the item already exists in the cart
+  var cartItem = null;
+  var cart = document.querySelector(".shopping-cart");
+  var cartItems = cart.querySelectorAll(".cart-item");
+  for (var i = 0; i < cartItems.length; i++) {
+    var name = cartItems[i].querySelector(".cart-item-details h4").innerText;
+    if (name === itemName) {
+      cartItem = cartItems[i];
+      break;
+    }
+  }
+
+  if (cartItem) {
+    // update the quantity of the existing cart item
+    var quantityElement = cartItem.querySelector(".cart-item-quantity");
+    var quantity = parseInt(quantityElement.innerText);
+    quantityElement.innerText = quantity + 1;
+
+    // update the cart total
+    updateCartTotal();
+  } else {
     // create a new cart item
-    var cartItem = document.createElement("div");
+    cartItem = document.createElement("div");
     cartItem.classList.add("cart-item");
     cartItem.innerHTML = `
       <img src="${itemImage}" alt="${itemName}">
       <div class="cart-item-details">
         <h4>${itemName}</h4>
-        <p>${itemPrice}</p>
+        <p data-price="${parseFloat(itemPrice.replace("$", ""))}">${itemPrice}</p>
       </div>
+      <span>Quantity: </span><span class="cart-item-quantity">1</span>
+      <br/>
       <button class="remove-item">Remove</button>
     `;
-  
+
     // add the item to the cart
-    var cart = document.querySelector(".shopping-cart");
-    var cartItems = cart.querySelector("#title");
-    cartItems.appendChild(cartItem);
-  
-    cartItem.querySelector(".remove-item").addEventListener("click", function() {
-        cartItem.remove();
-        updateCartTotal();
-      });
-      
+    var cartItemsContainer = cart.querySelector("#title");
+    cartItemsContainer.appendChild(cartItem);
+
+    cartItem.querySelector(".remove-item").addEventListener("click", function () {
+      cartItem.remove();
+      updateCartTotal();
+    });
+
     // update the cart total
     updateCartTotal();
   }
+}
 
 function updateCartTotal() {
-    var cart = document.querySelector(".shopping-cart");
-    var cartItems = cart.querySelectorAll(".cart-item");
+  var cart = document.querySelector(".shopping-cart");
+  var cartItems = cart.querySelectorAll(".cart-item");
 
-    // calculate the total price
-    var total = 0;
-    for (var i = 0; i < cartItems.length; i++) {
-        var itemPrice = cartItems[i].querySelector(".cart-item-details p").innerText;
-        total += parseFloat(itemPrice.replace("$", ""));
+  // calculate the total price and quantity
+  var total = 0;
+  var quantity = 0;
+  for (var i = 0; i < cartItems.length; i++) {
+    var itemPriceElement = cartItems[i].querySelector(".cart-item-details p");
+    var itemPrice = parseFloat(itemPriceElement.dataset.price);
+    var itemQuantity = parseInt(cartItems[i].querySelector(".cart-item-quantity").innerText);
+    total += itemPrice * itemQuantity;
+    quantity += itemQuantity;
+  }
+
+  // update the cart total and quantity
+  var cartTotal = cart.querySelector(".cart-total");
+  cartTotal.innerText = "Total (" + quantity + " items): $" + total.toFixed(2);
 }
-
-// update the cart total
-var cartTotal = cart.querySelector(".cart-total");
-cartTotal.innerText = "Total: $" + total.toFixed(2);
-}
-
-
-
-  
