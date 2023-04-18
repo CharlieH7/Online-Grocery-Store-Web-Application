a5868fe64e80774d2d090129b69c00ebbe0fd63a
CharlieH7 <hope91824@gmail.com>
2023-04-18 23:55:29 +1000

    add a cancel button on checkout.html + fix email format in sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -1,9 +1,20 @@
 function sendEmail() {
+  var name = document.getElementById("name").value;
+  var address = document.getElementById("address").value;
+  var suburb = document.getElementById("suburb").value;
+  var state = document.getElementById("state").value;
+  var country = document.getElementById("country").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
   Email.send({
     SecureToken: "05c03e4d-4f2c-40ef-bb2a-3a9ca6cb5e2f",
     To: document.getElementById("email").value,
     From: "hope91824@gmail.com",
     Subject: "Confirming your order",
-    Body: "Thank you for your order. We will contact you shortly.",
+    Body: `<p>Dear ${name},</p>
+    <p>Thank you for your order. <p>We have received your order</p><p>Your items will be shipped to the following address:</p>
+    <p>${address}, ${suburb}, ${state}, ${country}</p>
+    <p>Your phone number is ${phone} and your email is ${email}.</p>
+    <p>Best regards,</p><p>Fresh Choice Grocery</p>`,
   }).then((window.location.href = "thankyou.html"));
 }
