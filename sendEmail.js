f926669c8e2a24a4172bd616ab86e9fbb5bd88fa
CharlieH7 <hope91824@gmail.com>
2023-04-19 00:49:30 +1000

    add date in email

--- sendEmail.js
+++ sendEmail.js
@@ -6,13 +6,16 @@ function sendEmail() {
   var country = document.getElementById("country").value;
   var phone = document.getElementById("phone").value;
   var email = document.getElementById("email").value;
+  var date = new Date();
+
+
   Email.send({
     SecureToken: "05c03e4d-4f2c-40ef-bb2a-3a9ca6cb5e2f",
     To: document.getElementById("email").value,
     From: "hope91824@gmail.com",
     Subject: "Confirming your order",
     Body: `<p>Dear ${name},</p>
-    <p>Thank you for your order. <p>We have received your order</p><p>Your items will be shipped to the following address:</p>
+    <p>Thank you for your order. <p>We have received your order on ${date}</p><p>Your items will be shipped to the following address:</p>
     <p>${address}, ${suburb}, ${state}, ${country}</p>
     <p>Your phone number is ${phone} and your email is ${email}.</p>
     <p>Best regards,</p><p>Fresh Choice Grocery</p>`,
